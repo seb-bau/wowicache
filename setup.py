b1afe4d9acf82e7fdd17f276225cb62d0cbe4d7f
Sebastian Bauhaus <borsti@borstinet.de>
2024-04-15 13:58:12 +0000

    Fix module dependency

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='wowicache',
-    version='1.0.13',
+    version='1.0.15',
     description='OPENWOWI Wowiport SQLAlchemy overlay',
     url='https://github.com/seb-bau/wowicache',
     author='Sebastian Bauhaus',
